9eb9fe43f555a1932c8b36abc255e9caf536f077
rhit-wendella <wendella@rose-hulman.edu>
2023-01-31 00:39:50 +0000

    Finished the hw, just have to do the oscilliscope.

--- hw07/1wire_temp.sh
+++ hw07/1wire_temp.sh
@@ -7,7 +7,7 @@ TEMPERATURE3=`cat /sys/class/hwmon/hwmon2/temp1_input`
 
 echo "Temp 1 $(($TEMPERATURE1 / 1000)) degrees C"
 echo "Temp 2 $(($TEMPERATURE2 / 1000)) degrees C"
-echo "Temp 2 $(($TEMPERATURE3 / 1000)) degrees C"
+echo "Temp 3 $(($TEMPERATURE3 / 1000)) degrees C"
 echo "Temp 1 $((((($TEMPERATURE1 / 1000)*9)/5)+32)) degrees F"
 echo "Temp 2 $((((($TEMPERATURE2 / 1000)*9)/5)+32)) degrees F"
-echo "Temp 2 $((((($TEMPERATURE3 / 1000)*9)/5)+32)) degrees F"
+echo "Temp 3 $((((($TEMPERATURE3 / 1000)*9)/5)+32)) degrees F"
---
+++ hw08/README.md
@@ -0,0 +1,5 @@
+# HW06
+## Blinking an LED
+- Got the led to blink. Could not see the LED physically blink when I set the delay to 0. The pin was toggling at 12.5 Mhz. There was a bit of jittering and some overshoot, but it was stable.
+
+## 
